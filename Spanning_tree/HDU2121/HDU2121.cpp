f3007add0a7fef7119f2e98ed2a8e9d6426d4704
buerguoo <buerguoo@163.com>
2020-12-18 15:37:26 +0800

    one

--- Spanning_tree/HDU2121/HDU2121.cpp
+++ Spanning_tree/HDU2121/HDU2121.cpp
@@ -1,9 +1,9 @@
     /*
     Author:buerguoo
-    Time:
-    memory:
+    Time:390ms
+    memory:1876KB
 
-    Data:
+    Data:2020-12-18 15:32:56
     */
     #include <iostream>
     #include <cstring>
@@ -19,8 +19,9 @@
     typedef long long ll;
     const int MAXN = 1005;
     const int MAXM = 10010;
-    const int INF = 0x3f3f3f3f;
-    int in[MAXN], id[MAXN], pre[MAXN], vis[MAXN];
+    const ll INF = 1e17;
+    int in[MAXN], pre[MAXN], vis[MAXN], pos;
+    ll id[MAXN];
     struct Edge{
         int u, v, cost;
     }E[MAXM];
@@ -33,9 +34,12 @@
             for(int i = 0;i < m;++i)    if(E[i].v != E[i].u && E[i].cost < in[E[i].v]){
                 in[E[i].v] = E[i].cost;
                 pre[E[i].v] = E[i].u;
+                if(E[i].u == root)
+                    pos = i;    
             }
                 
-            for(int i = 0;i < n;++i) if(i != root && in[i] == INF) return -1;
+            for(int i = 0;i < n;++i) if(i != root && in[i] == INF) 
+                return -1;
 
             int tn = 0;
             memset(id, -1, sizeof(id));
@@ -60,14 +64,15 @@
                 if(id[i] == -1)
                     id[i] = tn++;
             
-            for(int i = 0;i < m;){
+            for(int i = 0;i < m;i++){
                 int v = E[i].v;
                 E[i].u = id[E[i].u];
                 E[i].v = id[E[i].v];
                 if(E[i].v != E[i].u)
-                    E[i++].cost -= in[v];
-                else
-                    swap(E[i], E[--m]);
+                    E[i].cost -= in[v];
+                //这样会打乱边和根的关系
+               // else
+                    //swap(E[i], E[--m]);
             }
             n = tn;
             root = id[root];
@@ -81,24 +86,25 @@
         int n, m;
         while(~scanf("%d%d", &n, &m)){
             int u, v, c;
+            ll sum = 0;
             for(int i = 0;i < m;++i){
                 scanf("%d%d%d", &u, &v, &c);
-                E[i].u = u;
-                E[i].v = v;
+                E[i].u = ++u;
+                E[i].v = ++v;
                 E[i].cost = c;
+                sum += c;
             }
-            int Min = INF, p = -1;
-            for(int i = 0;i < n;++i){
-                int zl = zhuliu(i, n, m, E);
-                //printf("%d \n", zl);
-                if(zl == -1) continue;
-                if(Min > zl){
-                    Min = zl;
-                    p = i;
-                }
-            }
-            if(p == -1) printf("impossible\n");
-            else printf("%d %d\n", Min, p);
+            sum++;
+            for(int i = m;i < n+m;++i){
+                E[i].u = 0;
+                E[i].v = i - m + 1;
+                E[i].cost = sum;
+            }           
+            ll ans = zhuliu(0, n+1, n+m, E);
+            if(ans == -1 || ans-sum>=sum)
+                printf("impossible\n\n");
+            else 
+                printf("%lld %d\n\n", ans-sum, pos-m);
         }
-        return 0;
+        return 0;   
     }
