df4ecaeed2f06f5d879f99d4481c1173887128e6
buerguoo <buerguoo@163.com>
2021-04-21 23:12:28 +0800

    WA

--- DP1/POJ1661/POJ1661.cpp
+++ DP1/POJ1661/POJ1661.cpp
@@ -8,7 +8,6 @@ memory:
 
 Data:
 */
-//待续... 我好弱...
 #include <iostream>
 #include <cstring>
 #include <string>
@@ -20,14 +19,13 @@ Data:
 #include <map>
 #include <algorithm>
 using namespace std;
+//WA 艹
 const int INF = 0x3f3f3f3f;
 const int MAXN = 1010;
 const int MAXM = 100010;
-int dp[MAXN];
-bool vis[MAXN];
+int dp[MAXN][2];
 struct platform{
     int a, b, h;
-    int pos;
     bool operator<(const platform &a)const{
         return h > a.h;
     }
@@ -40,38 +38,38 @@ int main()
     while(T--){
         scanf("%d %d %d %d", &n, &x, &y, &H);
         memset(dp, 0, sizeof(dp));
-        memset(vis, false, sizeof(vis));
         for(int i = 1;i <= n;++i)   scanf("%d %d %d", &P[i].a, &P[i].b, &P[i].h);
         P[0].a = P[0].b = x;
-        P[0].h = y; P[0].pos = x;
+        P[0].h = y;
         P[n+1].a = -INF, P[n+1].b = INF, P[n+1].h = 0;
         int dis = 0;
         sort(P+1, P+n);
         for(int i = 1;i <= n+1;++i){
-            dp[i] = INF;
-            for(int j = 0;j < i;++j) if(P[j].h - P[i].h <= H && !vis[j])
+            dp[i][0] = dp[i][1] = INF;
+            for(int j = i-1;j >= 0;--j) if(P[j].h - P[i].h <= H)
             {
                 if(P[i].a <= P[j].a && P[i].b >= P[j].a){
-                    dis = P[j].pos-P[j].a+P[j].h-P[i].h;
-                    printf("1 %d\n", dis);
-                    if(dp[i] > dp[j]+dis){
-                        dp[i] = dp[j] + dis;
-                        P[i].pos = P[j].a;
-                        vis[i] = true;
-                    }
-                }
-                if(P[i].a <= P[j].b && P[i].b >= P[j].b){
-                    dis = -P[j].pos+P[j].b+P[j].h-P[i].h;
-                    printf("2 %d\n", dis);
-                    if(dp[i] > dp[j]+dis){
-                        dp[i] = dp[j] + dis;
-                        P[i].pos = P[j].b;
-                        vis[i] = true;
+                    dis = P[j].a - P[i].a + P[j].h - P[i].h;
+                    if(dis > INF) dis = P[j].h - P[i].h;
+                    if(dp[i][0] > dp[j][0] + dis){
+                        dp[i][0] = dp[j][0] + dis;
+                        break;
                     }
                 }
             }
-         }   
-        printf("%d\n", dp[n+1]);
+            for(int j = i-1;j >= 0;--j) if(P[j].h - P[i].h <= H){
+                if(P[i].a <= P[j].b && P[i].b >= P[j].b){
+                    dis = P[i].b - P[j].b + P[j].h - P[i].h;
+                    if(dis > INF) dis = P[j].h - P[i].h;
+                    if(dp[i][1] > dp[j][1] + dis){
+                        dp[i][1] = dp[j][1] + dis;
+                        break;
+                    }
+                }
+            }
+        }
+        printf("%d %d\n", dp[n+1][0], dp[n+1][1]);
+        printf("%d\n", min(dp[n+1][0], dp[n+1][1]));
     }
     return 0;
 }
